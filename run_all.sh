6aab5307155745731ef849a488a11ebfea10d1fe
nancy <1575812469@qq.com>
2026-06-08 20:42:55 +0800

    1

--- run_all.sh
+++ run_all.sh
@@ -1,14 +1,14 @@
 set -e
 set -x
 
-export PARPOOL_NUM_WORKERS=8
+export PARPOOL_NUM_WORKERS=32
 cd /data4/yanxiaokai/project_energy_climate/globally_interconnected_reRun/Optimization_ssp126
-bash run_2050_pipeline.sh &> logs/run_2050.log
+bash run_full_pipeline.sh &> logs/run_full.log
 
-export PARPOOL_NUM_WORKERS=8
+export PARPOOL_NUM_WORKERS=32
 cd /data4/yanxiaokai/project_energy_climate/globally_interconnected_reRun/Optimization_ssp245
-bash run_2050_pipeline.sh &> logs/run_2050.log
+bash run_full_pipeline.sh &> logs/run_full.log
 
-export PARPOOL_NUM_WORKERS=8
+export PARPOOL_NUM_WORKERS=32
 cd /data4/yanxiaokai/project_energy_climate/globally_interconnected_reRun/Optimization_ssp560
-bash run_2050_pipeline.sh &> logs/run_2050.log
+bash run_full_pipeline.sh &> logs/run_full.log
